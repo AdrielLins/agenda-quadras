e69cf8c224bd5433e53382f8d2cfb482881a7d13
Adriel Lins Maciel <154188@upf.br>
2018-09-01 02:39:34 -0300

    Fix methods List, Update and Remove from users
    Changes variable name result -> doc

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,7 @@
 const User = require('../models/UserModel');
 var statusCode = require('http-status-codes');
 
-exports.CreateUser = function (req, res) {
+exports.CreateUser = function (req, res, next) {
 
     let user = new User({
         email: req.body.email,
@@ -13,24 +13,23 @@ exports.CreateUser = function (req, res) {
         adm: req.body.adm
     });
 
-    User.findOne({email: req.body.email}).exec(function(err, result) {
+    User.findOne({email: req.body.email}).exec(function(err, doc) {
         if (err){
             res.send(err);
             return;
         }
-        if (result.email) {
+        if (doc.email) {
             res.status(statusCode.UNAUTHORIZED).send('E-mail já em uso!');
-            return;        
         }
     });
     
-    User.findOne({cpf: req.body.cpf}).exec(function(err, result) {
+    User.findOne({cpf: req.body.cpf}).exec(function(err, doc) {
         if (err){
             res.send(err);
             return;
         }
-        if (result.cpf) {
-            res.status(statusCode.CONFLICT).send('CPF já em uso!')
+        if (doc.cpf) {
+            res.status(statusCode.CONFLICT).send('CPF já em uso!');
             return;
         }
     });
@@ -42,46 +41,85 @@ exports.CreateUser = function (req, res) {
         res.status(statusCode.CREATED).send('Usuário criado com sucesso!')
         return;
     })
-};
+}
 
 exports.ReadUser = function (req, res){
-    User.findOne({email: req.params.email}).exec(function(err, result) {
+    User.findOne({email: req.body.email}).exec(function(err, doc) {
         if (err) {
             res.send(err);
             return;
         } 
-        if(!result){
+        if(!doc){
             res.status(statusCode.NOT_FOUND).send('Usuário não encontrado!');
         }
         else {
-            res.send(result);
+            res.send(doc);
             return;
         }
     });
 };
-//experimental
 exports.ListUser = function (req, res){
-    try {
-        const results = User.find({});
-        console.log(results);
-        res.send(results);
-      } catch (err) {
-        throw err;
-      }
+    User.find({}).exec(function(err, doc) {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        if (!doc) {
+            res.status(statusCode.NOT_FOUND).send('Nenhum registro encontrado!');
+        }
+        else {
+            res.send(doc);
+            return;
+        }
+    });
 };
 
 exports.UpdateUser = function (req, res){
-    User.findByIdAndUpdate(req.params.id, {$set: req.body},
-    function(err, user){
-        if(err) return err;
-        res.send('User Updated!');
-    })
+    var userToUpdate = req.body.email
+    User.findOne({email: userToUpdate}).exec(function(err, doc) {
+        if (err){
+            res.send(err);
+            return;
+        }
+        if (!doc) {
+            res.status(statusCode.NOT_FOUND).send('E-mail não encontrado!');
+        } else {
+            User.prototype.findOneAndUpdate({email: userToUpdate},
+                {password:req.body.password,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                phone: req.body.phone,
+                adm:req.body.adm},
+                {new: true}, function(err, doc){
+                    if(err){
+                        res.send(doc);
+                        return;
+                    } else {
+                        res.send('Usuário atualizado!');
+                    }
+                });
+            }
+    });
 };
 
 exports.DeleteUser = function(req, res) {
-    User.findByIdAndRemove(req.params.id,
-    function(err, user){
-        if(err) return err;
-        res.send('User DELETED!');
-    })
+    var userToDelete = req.body.email;
+    User.findOne({email: userToDelete}).exec(function(err, doc) {
+        if (err){
+            res.send(err);
+            return;
+        }
+        if (!doc) {
+            res.status(statusCode.NOT_FOUND).send('E-mail não encontrado!');
+        } else {
+            User.deleteOne({email: userToDelete}, function(err, doc){
+                    if(err){
+                        res.send(doc);
+                        return;
+                    } else {
+                        res.send('Usuário excluido!');
+                    }
+                });
+            }
+    });
 };
