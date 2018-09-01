ce9791e61aa1f6bd0be1d4ba710752b7b096fd11
Adriel Lins Maciel <154188@upf.br>
2018-09-01 02:37:57 -0300

    fix [ref] removes unnecessary comments

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 // Configurações de conexão com o banco
 
 const mongoose = require('mongoose');
-// const uniqueValidator = require('mongoose-unique-validator');
 const mongoDBUrl = 'mongodb://admin:masterkey1@ds163181.mlab.com:63181/adrielproject';
-//const mongoDB = process.env.MONGODB_URI || mongoDBUrl;
 
 mongoose.connect(mongoDBUrl, { useMongoClient: true  });
 mongoose.Promise = global.Promise;
@@ -14,4 +12,3 @@ console.log('Connecting DataBase adrielproject');
 connectionDB.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = connectionDB;
-// module.exports = uniqueValidator;
