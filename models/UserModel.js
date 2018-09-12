51f81b529b434dbbddcd96ed6f57fe78a44810b2
Adriel Lins Maciel <adriel.maciel@compasso.com.br>
2018-09-12 18:38:34 -0300

    Fix password type on user crud Number->String

--- models/UserModel.js
+++ models/UserModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
     email:{type: String, required: true},
-    password:{type: Number, required: true},
+    password:{type: String, required: true, max: 20},
     firstName: {type: String, required: true, max: 50},
     lastName: {type: String, required: true, max: 100},
     cpf: {type: String, required: true, max: 30},
