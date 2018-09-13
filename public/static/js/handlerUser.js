18ec6453ffb968bfaebfb2437072b30b5209c687
Adriel Lins Maciel <adriel.maciel@compasso.com.br>
2018-09-13 18:09:39 -0300

    Add the login function using Ajax

--- public/static/js/handlerUser.js
+++ public/static/js/handlerUser.js
@@ -1,8 +1,31 @@
-function login () {
-    alertify.error('Preencha os campos animal!');
-//   alertify.alert('Alert Title', 'Alert Message!', function(){ alertify.error('Ok'); });
-    var data = [];
-    data = $("#userEmail");
-    data = $("#userPassword");
-    console.log(data);
+function login() {
+    var email = $('#userEmail').val();
+    var senha = $('#userPassword').val();
+
+    if (!email) {
+        alertify.alert('Atenção!', 'Favor preencha o campo de email.');
+        return
+    }
+    if (!senha) {
+        alertify.alert('Atenção!', 'Favor preencha o campo de senha.');
+        return
+    }
+
+    ajaxData = {};
+    ajaxData['email'] = email;
+    ajaxData['password'] = senha;
+
+    $.ajax({
+        type: "POST", url: "/api/users/find/",
+        data: ajaxData
+    }).done(function (res) {
+        console.log(res)
+        if (!res) {
+            alertify.alert('Atenção!', 'E-mail ou senha inválidos.');
+            return
+        }
+    }).fail(function (err) {
+        alertify.alert('Erro', 'Não foi possível realizar esta solicitação.');
+        return
+    });
 }
