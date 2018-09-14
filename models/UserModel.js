c5f595a542ae9d7921ab5ba3456cdedfc5a615c9
Adriel Lins Maciel <154188@upf.br>
2018-09-13 23:30:53 -0300

    Fix change phone type Number->String
    Mask was sending values with more than just
    numbers.
    Add max length on fields.

--- models/UserModel.js
+++ models/UserModel.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
-    email:{type: String, required: true},
+    email:{type: String, required: true, max: 50},
     password:{type: String, required: true, max: 20},
     firstName: {type: String, required: true, max: 50},
     lastName: {type: String, required: true, max: 100},
     cpf: {type: String, required: true, max: 30},
-    phone: {type: Number, required: true},
+    phone: {type: String, required: true, max: 30},
     adm: {type: Boolean, required: true}
 });
 
