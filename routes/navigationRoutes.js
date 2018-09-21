dfeba22d2fd2f84891beaf9611bb5b351f1d37ff
Adriel Lins Maciel <adriel.maciel@compasso.com.br>
2018-09-21 12:36:33 -0300

    Fix Adjust navigationRoutes redirects
    To avoid infinite loops

--- routes/navigationRoutes.js
+++ routes/navigationRoutes.js
@@ -3,24 +3,23 @@ var router = express.Router();
 const bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: false }));
 
-// middleware function, check if user is already logged in
-var sessionChecker = (req, res, next) => {
-    if (!req.session.user && !req.cookies.user_sid) {
-        res.redirect('./login.html');
+//check if user is already logged in while goin to home page
+router.get('/', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('./home.html');
     } else {
-        next();
+        res.redirect('./login.html');
     }
-};
-
-//check if user is already logged in while goin to home page
-router.get('/', sessionChecker, (req, res) => {
-    res.redirect('./home.html');
-});
-//check if user is already logged in while goin to home page
-router.get('/home.html', sessionChecker, (req, res) => {
-    res.redirect('./home.html');
 });
 
+//check if user is already logged in while goin to home page
+router.get('/home.html', (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        next();
+    } else {
+        res.redirect('./login.html');
+    }
+});
 
 // route for user logout
 router.get('/logout', (req, res) => {
