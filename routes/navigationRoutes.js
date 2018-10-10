9b0c67e8315826debf84d40467c952cb0f88b356
Adriel Lins Maciel <154188@upf.br>
2018-10-10 20:48:52 -0300

    Add Field management for administrator

--- routes/navigationRoutes.js
+++ routes/navigationRoutes.js
@@ -40,6 +40,15 @@ router.get('/manageUser.html', (req, res, next) => {
     }
 });
 
+//check if user is already logged in while goin to manageUser
+router.get('/manageField.html', (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        next();
+    } else {
+        res.redirect('./login.html');
+    }
+});
+
 // route for user logout
 router.get('/logout', (req, res) => {
     if (req.session.user && req.cookies.user_sid) {
