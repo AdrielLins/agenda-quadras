73e9edead15e79b50e5024a8c02550ebc89078d0
Adriel Lins Maciel <154188@upf.br>
2018-11-02 13:34:58 -0300

    Add initiate development to user agenda
    Added routes and changed links on Html files

--- routes/navigationRoutes.js
+++ routes/navigationRoutes.js
@@ -66,6 +66,14 @@ router.get('/manageAgenda.html', (req, res, next) => {
         res.redirect('./home.html');
     }
 });
+//check if user is already logged in
+router.get('/agenda.html', (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        next();
+    } else {
+        res.redirect('./home.html');
+    }
+});
 
 // route for user logout
 router.get('/logout', (req, res) => {
