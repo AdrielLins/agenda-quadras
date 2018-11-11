fae70e7901fbafe47008b660ce47bacbfe734849
Adriel Lins Maciel <154188@upf.br>
2018-11-11 03:19:37 -0200

    Fix fix monetary values on front
    Changes "valor" type, Number->String.

--- models/AgendaModel.js
+++ models/AgendaModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 let AgendaSchema = new Schema({
     dateAgenda: { type: Date, required: true },
     quadraNumero: { type: Number, required: true },
-    esporteValor: { type: Number, required: true },
+    esporteValor: { type: String, required: true },
     esporteModalidade: { type: String, required: true },
     status: { type: String, required: true },
     userEmail: { type: String },
